7d99609b3121adf96a2d61c2aa9637dd82aba1f0
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-09-13 10:06:35 -0400

    update gitignore

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  // API_ENDPOINT: 'https://desolate-anchorage-33266.herokuapp.com/api'
+  API_ENDPOINT: 'https://desolate-anchorage-33266.herokuapp.com/api',
   TOKEN_KEY: 'super-snippets-client-auth-token'
 };
