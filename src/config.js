559b6c8b442e854c8c15ad099e5d978b1ad5b392
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-09-23 18:05:56 -0400

    add favicon react package

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'super-snippets-client-auth-token'
-};
-
 // export default {
-//   API_ENDPOINT: 'https://desolate-anchorage-33266.herokuapp.com/api',
+//   API_ENDPOINT: 'http://localhost:8000/api',
 //   TOKEN_KEY: 'super-snippets-client-auth-token'
 // };
+
+export default {
+  API_ENDPOINT: 'https://desolate-anchorage-33266.herokuapp.com/api',
+  TOKEN_KEY: 'super-snippets-client-auth-token'
+};
